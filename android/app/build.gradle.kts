66b5a8d2804720f00dec72a5f498ffc56bb4b260
itskdey <meanpheakdey069@gmail.com>
2026-08-22 22:52:43 +0700

    feat: configure release signing and update project documentation screenshots

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,16 @@
+// TOP of android/app/build.gradle.kts
+// Put BEFORE plugins { }
+import java.io.FileInputStream
+import java.util.Properties
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -26,11 +39,27 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storePassword = keystoreProperties["storePassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let {
+                file(it as String)
+            }
         }
     }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+
+
+    
 }
 
 flutter {
